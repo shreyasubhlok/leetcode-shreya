8d9a1d2ea3ce71c4962a4ace55d8ec5684701c00
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-19 19:37:51 -0700

    Time: 52 ms (89.66%), Space: 17.7 MB (54.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,37 @@
 class Solution:
-
-    def twoSum(self,nums:List[int],target:int)->List[int]:
-        myMap={}
+    # Time complexity is o(n)
+    # Space complexity is o(n) - coz of dictionary myMap
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myMap = {}  # Initialize an empty dictionary to store indicies of each nums[i]
         for i in range(len(nums)):
-            diff= target-nums[i]
-            if diff in myMap:
-                return [myMap[diff],i]
-            myMap[nums[i]]=i
-        return []
-            
+            difference = target - nums[i]  # calculate the difference
+            if difference in myMap:
+                # if difference already exists in myMap then return current indicies and no that matches the difference
+                return [
+                    i,
+                    myMap[difference],
+                ]  # or return [i,myMap.get(difference)] -this is also correct
+            myMap[nums[i]] = i
 
+
+"""
+myMap[difference] VS myMap.get(difference)
+myMap[difference] -> raise a keyError if difference not found in dictionary
+myMap.get(difference) -> return None if difference not found in dictionary, rather then raising the keyError
+For your current code - Since you are already checking whether difference exists in myMap or not, so you can use both.
+But stick to myMap[difference] as it's more direct and efficient.
+Another way- same time and space
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myDict ={}
+        res=[-1,-1]
+        for i in range(len(nums)):
+            diff = target-nums[i]
+            if diff in myDict:
+                res[0]=i
+                res[1]=myDict[diff]
+            myDict[nums[i]]=i   
+        return res     
+'''
+"""

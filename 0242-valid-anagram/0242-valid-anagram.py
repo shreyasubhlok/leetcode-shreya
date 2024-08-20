4c759ec8324421c2ffa3e3bde5dabdf734add331
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-19 23:05:58 -0700

    Time: 51 ms (46.51%), Space: 16.9 MB (45.80%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,29 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
+        # time is o(n)
+        # space is o(n)
+        if len(s) != len(t):
             return False
-        
-        myMap={}
+
+        myMap = {}
+        # Count occurrences of each character in string s
         for char in s:
             if char in myMap:
-                myMap[char]=myMap[char]+1
+                myMap[char] = myMap[char] + 1  # myMap[char] = myMap.get(char) + 1 -> this is also correct in python
             else:
-                myMap[char]=1
-        
+                myMap[char] = 1
+
+        # Compare counts of characters in string t
         for char in t:
             if char in myMap:
-                myMap[char]=myMap[char]-1
-                if myMap[char]==0:
-                    del myMap[char]
+                myMap[char] = myMap[char] - 1
+                if myMap[char] == 0:
+                    del myMap[char] #delete a specific key-value pair from a dictionary
             else:
                 return False
-        
-        return len(myMap)==0
-            
+
+        # Check if all characters are matched
+        if len(myMap) == 0:
+            return True
+        else:
+            return False

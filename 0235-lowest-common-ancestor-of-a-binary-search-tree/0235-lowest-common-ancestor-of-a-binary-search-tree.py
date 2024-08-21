93853f6111661239e9ebd3c4ba0779e498eec091
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-20 21:13:51 -0700

    Time: 51 ms (78.45%), Space: 20.3 MB (29.42%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,16 +5,20 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root is None:
+    def lowestCommonAncestor(self, root: "TreeNode", p: "TreeNode", q: "TreeNode") -> "TreeNode":
+        # time complexity: O(h)<o(n) - h is height of BST and n is no of nodes. In worst case o(h)=o(n) when all nodes on left (linear tree)
+        # space complexity : o(n)- due to the space used by the recursive call stack
+        if root == None:  # Base case: If root is null, return null
             return None
-        
-        curr=root.val
-        if curr<p.val and curr<q.val:
-            return self.lowestCommonAncestor(root.right,p,q)
-            
-        if curr>p.val and curr>q.val:
-            return self.lowestCommonAncestor(root.left,p,q)
-            
+
+        curr = root.val
+        if curr < p.val and curr < q.val:  # Case 1: Both nodes are on the right subtree
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        if curr > p.val and curr > q.val:  # Case 2: Both nodes are on the left subtree
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        # Case 3 and 4: One node is on the left and the other on the right, or one of the nodes is the current root
         return root

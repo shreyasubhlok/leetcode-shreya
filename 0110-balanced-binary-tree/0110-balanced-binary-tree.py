14c829167017c17aa10b8a4d5828214c3d98b9f7
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-21 12:43:50 -0700

    Time: 37 ms (92.51%), Space: 17.5 MB (86.94%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,8 +5,13 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    '''
+    Balanced binary tree meaning the heght difference between left and subtrees is AT MOST 1
+    Time complexity: O(N),N is no of nodes in tree. Coz each node is visited once, the overall time complexity is linear i.e O(N).
+    Space Complexity: O(H),H is the height of the tree. This could be bad as O(N) in a skewed tree or O(log N) in a balanced tree.
+    '''
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return self.Height(root) != -1
+        return self.Height(root) != -1 #The tree is balanced if the height function does not return -1
         
     def Height(self, root):
         if root is None:
@@ -15,8 +20,12 @@ class Solution:
         left_height = self.Height(root.left)
         right_height = self.Height(root.right)
 
-        # Check if the subtree is balanced
-        if left_height == -1 or right_height == -1 or abs(left_height - right_height) > 1:
+        # If either subtree is unbalanced, propagate the -1 upwards
+        if left_height == -1 or right_height == -1:
+            return -1
+
+        # If the current node is unbalanced (height difference > 1), return -1
+        if abs(left_height - right_height) > 1:
             return -1
 
         return max(left_height, right_height) + 1

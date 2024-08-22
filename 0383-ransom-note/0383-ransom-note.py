8fb86b2199be243ccacae63f31cad9fe611aafb1
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-21 17:39:49 -0700

    Time: 53 ms (69.62%), Space: 16.7 MB (24.50%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,7 +1,11 @@
 class Solution:
+     #time is o(n+m)=o(n)
+    #space is o(1) 
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote)>len(magazine):
+            return False
+
         myMap={}
-        
         for char in ransomNote:
             if char in myMap:
                 myMap[char]=1+myMap[char]
@@ -15,4 +19,19 @@ class Solution:
                     del myMap[char]
         
         return len(myMap)==0
+        
+        
                 
+#More optimized - optional :Time and space both same for both appraches. So any approach is fine
+'''
+        #from collections import Counter
+        #magazine_count = Counter(magazine) creates a Counter object that counts the occurrences of each character in the magazine string, 
+        providing a straightforward way to analyze and manipulate frequency data in Python.
+        magazine_counts = Counter(magazine)
+        for ch in ransomNote:
+            if magazine_counts[ch]>0:
+                magazine_counts[ch]=magazine_counts[ch]-1
+            else:
+                return False
+        return True
+'''        

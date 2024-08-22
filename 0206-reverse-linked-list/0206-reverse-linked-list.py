d009f6dcd46789f212594c6378ae5ebaae957be7
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-21 18:34:38 -0700

    Time: 43 ms (17.47%), Space: 17.8 MB (26.54%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,14 +4,22 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    #time is o(n) 
+    #space is o(1) - using pointers like prev or current is o(1) 
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev=None
-        curr=head
-        while curr!=None:
-            temp=curr.next
-            curr.next=prev
-            prev=curr
-            curr=temp
+        if head is None or head.next is None:
+            return head
+
+        prev = None
+        '''
+         In Python, when you pass an object (like head in this case) to a function, you're passing a reference to that object.
+         Any  modifications made to the object inside the function will affect the original object outside the function. So use current
+        '''
+        current = head
+        while current != None:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+
         return prev
-        
-            

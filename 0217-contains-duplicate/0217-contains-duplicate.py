b32d57cf84aac0030d2cea6ab88d69a465ce7314
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-21 23:34:09 -0700

    Time: 393 ms (99.25%), Space: 31.9 MB (47.75%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,5 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Approach 1 (preferred apprach) to use set - better approach
+        # time is o(n) and space is o(n)...on time is better then o nlogn which will be there in case of                 sorting
         mySet=set()
         for num in nums:
             if num in mySet:
@@ -7,4 +9,16 @@ class Solution:
             else:
                 mySet.add(num)
         
+        return False
+    
+    # Apprach 2
+    def containsDuplicateUsingSorting(self, nums: List[int]) -> bool:
+        #Approach 2 : sorting
+        #Sorting is not optimal: Sorting the list takes O(n log n) time, which is less efficient than using a          set which takes O(n) time.
+        # Time complexity is O(n log n) due to sorting
+        # Space complexity is O(1) if we don't count the input array
+        nums.sort()
+        for i in range(len(nums) - 1):  # Iterate until the second last element
+            if nums[i] == nums[i + 1]:
+                return True
         return False

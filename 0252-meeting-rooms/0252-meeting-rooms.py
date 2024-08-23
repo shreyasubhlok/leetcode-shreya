491e35e143096b9e818d4555d356ac974b95c371
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-22 21:55:36 -0700

    Time: 78 ms (22.34%), Space: 20.1 MB (6.63%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,13 +1,14 @@
 class Solution:
+
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        if len(intervals)==1:
-            return True
-        
-        intervals.sort()
-        for i in range(len(intervals)-1):
-            if intervals[i][1] > intervals[i+1][0]:
-                return False
+        # sort the intervals based on start time
+        intervals = sorted(intervals, key=lambda interval: interval[0])
+        print(intervals)
+        for i in range(len(intervals) - 1):
+            # Check if the current meeting's end time is greater than the next meeting's start time
+            if intervals[i][1] > intervals[i + 1][0]:
+                return False  # If true, an overlap is detected, meaning it's impossible to attend both meetings
             
-        return True
+        return True  # No overlap, that means can attend all meetings
         
         

0b1c14b994260bc525b194f2440e05286b0204a8
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-26 01:58:34 -0700

    Time: 105 ms (81.40%), Space: 19 MB (92.53%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,15 @@
 class Solution:
+    #Preferred Solution-Bitwise XOR
+    '''
+    Time:o(n) and space o(1)
+    Logic:
+     # XOR each number with the current result
+     # This works because XOR of a number with itself cancels out to 0,
+     # and XOR with 0 leaves the number unchanged. 
+     # Thus, all numbers appearing twice cancel out, leaving only the unique number.
+    '''
     def singleNumber(self, nums: List[int]) -> int:
-        ans=0
+        ans=0 # Initialize the result variable to 0
         for num in nums:
             ans=ans^num
         return ans

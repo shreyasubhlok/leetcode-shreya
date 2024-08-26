3fa5c7f482f77048af2e4e8d612e8d4f67cf1b6a
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-26 01:55:58 -0700

    Time: 110 ms (57.93%), Space: 19.1 MB (54.16%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        ans=0
+        for num in nums:
+            ans=ans^num
+        return ans
+        
+    
+    #time complexity = o(n)
+    #space complexity =o(n)
+    def singleNumberUsingHashSet(self, nums: List[int]) -> int:
         mySet=set()
         for num in nums:
             if num in mySet:

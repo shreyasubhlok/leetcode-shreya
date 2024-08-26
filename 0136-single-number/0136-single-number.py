d27a4440127bed94a98a9597dc8b5ac05b5a7f51
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-26 02:00:34 -0700

    Time: 113 ms (42.96%), Space: 19.1 MB (18.67%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -7,6 +7,11 @@ class Solution:
      # This works because XOR of a number with itself cancels out to 0,
      # and XOR with 0 leaves the number unchanged. 
      # Thus, all numbers appearing twice cancel out, leaving only the unique number.
+     Example- 
+     nums=[2,2,1]
+     i=0, ans=ans^nums[0]=0^2=2
+     i=1, ans=ans^nums[1]=2^2=0
+     i=2, ans=ans^nums[2]=0^1=1 (all duplictes will cancel out in the end)
     '''
     def singleNumber(self, nums: List[int]) -> int:
         ans=0 # Initialize the result variable to 0

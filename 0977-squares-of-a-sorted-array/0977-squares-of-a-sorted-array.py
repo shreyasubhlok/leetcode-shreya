9aea077ee10437c5d51d61443ad3d60eb9d598be
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-28 21:31:41 -0700

    Time: 153 ms (62.12%), Space: 18.8 MB (58.91%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -20,3 +20,58 @@ class Solution:
                 end = end - 1  # Move the end pointer to the left.
 
         return res
+    
+'''
+Dry run -
+  nums = [-4, -1, 0, 3, 10]
+           S             E
+  res = [0 , 0 ,0, 0 ,0]
+                      i
+iteration 0:
+  nums = [-4, -1, 0, 3, 10]
+           S         E
+  res = [0 , 0 ,0, 0 ,100]
+                   i
+abs(-4)>abs(10) ? No    res[4]=10*10   End=end-1
+
+iteration 1:
+  nums = [-4, -1, 0, 3, 10]
+               S     E
+  res = [0 , 0 ,0, 16 ,100]
+                i
+abs(-4)>abs(3) ? yes    res[3]=4*4   start=start+1
+
+iteration 2:
+  nums = [-4, -1, 0, 3, 10]
+               S     E
+  res = [0 , 0 ,9, 16 ,100]
+                i
+abs(-1)>abs(3) ? No    res[2]=3*3   End=end-1
+
+iteration 1:
+  nums = [-4, -1, 0, 3, 10]
+               S  E
+  res = [0 , 1 ,9, 16 ,100]
+         i
+abs(-1)>abs(0) ? No    res[1]=1*1   start=start+1
+
+
+iteration 0:
+  nums = [-4, -1, 0, 3, 10]
+               S  E
+  res = [0 , 1 ,9, 16 ,100]
+         i
+abs(0)>abs(0) ? No    res[0]=0*0   End=end-1
+
+  res = [0 , 1 ,9, 16 ,100] OUTPUT
+
+
+
+Additional=>
+ for i in range(len(nums) - 1, -1, -1) : 
+ Explanation
+  for i in range(start,stop,step):
+  len(nums) -> 1 (start) -> starting from reverse order
+  -1 (stop) ->ensures that the loop includes index 0 (the first element) and stops before reaching -1
+  -1(step) ->This step value of -1 indicates that the loop should move in reverse, decreasing the index by 1 in each iteration
+'''

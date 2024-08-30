055943d0fcb71dc8bec37fe40f6eb3479c89cf40
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-29 19:01:37 -0700

    Time: 56 ms (44.80%), Space: 17.7 MB (96.98%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -5,21 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        if len(nums)==0:
+    """
+      time complexity = o(n), The sortedArrayToBST method processes each element of the 
+      nums array exactly once
+      space complexity= olog(n), O(log n) for a balanced binary search tree (BST): The recursion
+      stack depth is proportional to the height of the tree. For a balanced BST, the height is
+      O(log n), where n is the number of elements in the array.
+    """
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if len(nums) == 0:
             return None
-        return self.isHelper(0,len(nums)-1,nums)
-    
-    def isHelper(self,left:int,right:int,nums:List[int])->TreeNode:
-        if left>right:
+        return self.isHelper(nums, 0, len(nums) - 1)
+
+    def isHelper(self, nums: List[int], left: int, right: int) -> TreeNode:
+        '''
+        Helper function to build the BST recursively.
+
+        :param nums: List of integers.
+        :param left: Left index of the current subarray.
+        :param right: Right index of the current subarray.
+        :return: Root of the subtree built from the subarray.
+        '''
+        # Base case: if the left index exceeds the right index, return None
+        if left > right:
             return None
-        
-        mid = (left+right) // 2
-        
-        root=TreeNode(nums[mid])
-        root.left=self.isHelper(left,mid-1,nums)
-        root.right=self.isHelper(mid+1,right,nums)
-        
-        
+
+        mid = (left + right) // 2
+        root = TreeNode(nums[mid]) 
+        root.left = self.isHelper(nums, left, mid - 1)
+        root.right = self.isHelper(nums, mid + 1, right)
+
         return root
-        

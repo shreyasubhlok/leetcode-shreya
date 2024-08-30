6500d7f591868439bbda2bdfc25b3ab52ea1738b
shreyasubhlok <shreyasubhlok@gmail.com>
2024-08-29 19:44:59 -0700

    Time: 74 ms (65.28%), Space: 16.8 MB (51.89%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -13,12 +13,17 @@ class Solution:
         if root is None:
             return False  # If root is empty but subRoot is not, subRoot can't be a subtree
 
-        # Check if the current root matches subRoot, or if subRoot is a subtree of either the left or right child of root.
-        return self.isSame(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        # Check if the current root matches subRoot, if yes, return True
+        if self.isSame(root , subRoot):
+            return True
+        
+        #check if subRoot is a subtree of either the left or right child of root?
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
     def isSame(self, r: TreeNode, s: TreeNode) -> bool:
         if r == None and s == None:
             return True  # Both trees are empty, so they are identical
+        
         if r == None or s == None:
             return False  # One tree is empty and the other is not, so they are not identical.
 

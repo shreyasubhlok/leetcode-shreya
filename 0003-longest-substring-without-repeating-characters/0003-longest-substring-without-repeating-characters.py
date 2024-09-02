79c9e86d2bfc1bdbd89bba631ab7ebca83c146ca
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-01 22:43:12 -0700

    Time: 51 ms (76.91%), Space: 16.7 MB (41.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,25 @@
 class Solution:
+    # Time is o(n) and space is o(n)
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s)==0:
+        if len(s) == 0:
             return 0
-        
-        i=0
-        maxLen=0
-        myMap={}
-        
+
+        i = 0  # Initialize the starting index of the current window
+        maxLen = 0
+        myMap = {}
+
         for j in range(len(s)):
+            # If the current character is already in the map, it means we found a repeating character
             if s[j] in myMap:
-                i=max(i,myMap[s[j]]+1)
-            myMap[s[j]]=j
-            currLen=j-i+1
-            maxLen=max(maxLen,currLen)
-            
-        
+                # Update the start of the window (i) to be the maximum of the current i
+                # and one position after the last occurrence of the current character
+                # This ensures that i only moves forward and doesn't move backward
+                i = max(i, myMap[s[j]] + 1)
+
+            myMap[s[j]] = j  # update the current character's index in the map
+            currLen = j - i + 1  # Calculate the length of the current window (substring without repeating characters)
+            maxLen = max(maxLen, currLen)
+
         return maxLen
+
             

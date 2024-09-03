4f3190093e630eab45f097c4f0b74a522bb49a62
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-02 20:04:04 -0700

    Time: 73 ms (16.46%), Space: 17 MB (96.52%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,25 +1,34 @@
+
 class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        if len(tokens)==0:
+    # time is o(n):Pushing and popping operations from the stack are O(1), so overall, the time complexity is O(n), where n is the number of tokens.
+    # space is o(n):In the worst case, all tokens could be stored in the stack, so the space complexity is O(n), where n is the number of tokens.
+    def evalRPN(self, tokens: List[int]) -> int:
+        if len(tokens) == 0:
             return 0
-        stack=[]
-        res=0
+
+        stack = []
         for t in tokens:
+            # Check if the token is an operator
             if t in "+-*/":
-                b=int(stack.pop())
-                a=int(stack.pop())
-                if t=='+':
-                    res=a+b
-                elif t=='-':
-                    res=a-b
-                elif t=='*':
-                    res=a*b
+                # Pop the top two elements from the stack; b is the second operand, a is the first operand
+                b = int(stack.pop())
+                a = int(stack.pop())
+
+                # Perform the appropriate arithmetic operation based on the operator
+                if t == "+":
+                    res = a + b
+                elif t == "-":
+                    res = a - b
+                elif t == "*":
+                    res = a * b
                 else:
-                    res=int(a/b)
-                stack.append(res)
+                    # Perform integer division and truncate towards zero to handle cases like ["0", "3", "/"]
+                    res = int(a / b)
+                stack.append(res)  # push res to stack
             else:
-                stack.append(int(t))
-        return stack[0]
-                
+                stack.append(int(t))  # convert to int and push it to stack to perform arithmatic operation
+        return stack[0]  # final result will be only one element left in stack which is stack[0]
+
+
                     
                 

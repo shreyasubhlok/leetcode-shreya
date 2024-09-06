a1ce7d47ffb422abdf5e96f84fbf42df6eab4451
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-06 03:00:42 -0700

    Time: 46 ms (17.41%), Space: 16.9 MB (77.07%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -8,26 +8,36 @@ class Node:
 
 from typing import Optional
 class Solution:
+    # BFS function to clone the graph
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # Check if the input graph is empty
         if node is None:
-            return node
-        clonedDict={}
-        queue=deque()
-        queue.append(node)
-        
-        clonedDict[node]=Node(node.val)
-        
-        while queue :
-            currNode=queue.popleft()
-            
-            for neighbor in currNode.neighbors:
-                if neighbor not in clonedDict:
-                    clonedDict[neighbor]=Node(neighbor.val)
+            return node # Return None if there's no graph to clone
+
+        # Dictionary to store cloned nodes. The key is the original node,  and the value is the corresponding cloned node.
+        clonedNodesMap = {}
+
+        # Initialize a queue for BFS traversal
+        queue = deque()
+        queue.append(node)  # Start BFS with the given node
+
+        # Clone the start node and put it in the clonedNodeMap dictionary
+        #The key would be the original node (let's say node with value 1). The value would be the newly cloned node, which is Node(1).
+        clonedNodesMap[node] = Node(node.val)
+
+        # BFS traversal
+        while queue:
+            currentNode = queue.popleft() # Pop the front node from the queue
+
+            # Iterate over all neighbors of the current node
+            for neighbor in currentNode.neighbors:
+                if neighbor not in clonedNodesMap: # If the neighbor hasn't been cloned yet
+                    # Clone the neighbor and add it to the dictionary
+                    clonedNodesMap[neighbor] = Node(neighbor.val)
+                    # Add the neighbor to the queue for BFS
                     queue.append(neighbor)
-                
-                clonedDict[currNode].neighbors.append(clonedDict[neighbor])
-        
-        return clonedDict[node]
-        
-        
-        
+
+                # Add the cloned neighbor to the current cloned node's neighbors list
+                clonedNodesMap[currentNode].neighbors.append(clonedNodesMap[neighbor])
+
+        return clonedNodesMap[node]  # Return the cloned node corresponding to the input node

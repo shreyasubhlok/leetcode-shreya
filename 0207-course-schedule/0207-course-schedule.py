230ce63b3d402dabe7e904c462485f91ddbb89a0
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-08 18:39:33 -0700

    Time: 86 ms (77.40%), Space: 18.3 MB (52.40%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,36 +1,28 @@
-from collections import defaultdict, deque
-from typing import List
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Create an adjacency list to represent the graph
-        graph = defaultdict(list)
-        # Initialize in-degrees
-        inDegree = [0] * numCourses
+        graph=defaultdict(list)
+        inDegree=[0]*numCourses
+        for course,prereq in prerequisites:
+            graph[prereq].append(course)
+            inDegree[course]=inDegree[course]+1
+    
+            
+        topOrder=[]
         
-        # Build the graph and calculate in-degrees
-        for course, prereq in prerequisites:
-            graph[prereq].append(course)  # prereq -> course, directed edge
-            inDegree[course] += 1  # Increment in-degree for the course
-        
-        # Initialize a queue with all nodes having zero in-degrees
-        queue = deque()
+        queue=deque()
         for i in range(numCourses):
-            if inDegree[i] == 0:
+            if inDegree[i]==0:
                 queue.append(i)
         
-        # Process the nodes in the queue
-        topOrder = []
         while queue:
-            currCourse = queue.popleft()
+            currCourse=queue.popleft()
             topOrder.append(currCourse)
             
-            # Decrease the in-degree of neighboring nodes
-            for neighbor in graph[currCourse]:
-                inDegree[neighbor] -= 1
-                # If in-degree becomes zero, add it to the queue
-                if inDegree[neighbor] == 0:
-                    queue.append(neighbor)
+            for prereq in graph[currCourse]:
+                inDegree[prereq]=inDegree[prereq]-1
+                if inDegree[prereq]==0:
+                    queue.append(prereq)
         
-        # Check if topological sort includes all nodes
-        return len(topOrder) == numCourses
+        return len(topOrder)==numCourses
+        
+        

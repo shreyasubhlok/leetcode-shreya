3a2fc5e40461db52c88f6b6b1b28055daf567c02
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-11 20:15:24 -0700

    Time: 261 ms (80.06%), Space: 25.8 MB (48.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,14 @@
 class Solution:
+    # Time: o(n)
+    # space: o(1), The space complexity is O(1) for auxiliary space since the res array is considered the output and doesn't count toward extra space as given in question.
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res=[1]*len(nums)
-        prefix=1
+        res = [1] * len(nums)
+        prefix = 1
         for i in range(len(nums)):
-            res[i]=prefix
-            prefix=prefix*nums[i]
-        
-        postfix=1
-        for i in range(len(nums)-1,-1,-1):
-            res[i]=res[i]*postfix
-            postfix=postfix*nums[i]
-        
+            res[i] = prefix  # Store the current prefix product at res[i]
+            prefix = prefix * nums[i]  # Update the prefix product by multiplying with nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):  # Traverse the array in reverse
+            res[i] = res[i] * postfix  # Multiply the current res[i] (which holds the prefix product) with the postfix product
+            postfix = postfix * nums[i]  # Update the postfix product by multiplying with nums[i]
         return res

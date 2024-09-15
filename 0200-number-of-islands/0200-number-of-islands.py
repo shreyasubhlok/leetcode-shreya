ccd267a1516f6fb649c8a4bf35da91f24000f14d
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-14 19:59:32 -0700

    Time: 226 ms (89.15%), Space: 18.8 MB (82.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,8 @@
 class Solution:
+    #time is o(m*n), m rows and n cols
+    #space is o(m*n) worst case,n case that the grid map is filled with lands where DFS goes by m*n deep
     def numIslands(self, grid: List[List[str]]) -> int:
+        # Check if the grid is empty or has no columns
         if not grid or not grid[0]:
             return 0
         
@@ -9,17 +12,21 @@ class Solution:
         
         for i in range(row):
             for j in range(col):
-                if grid[i][j]=="1":
+                # If the cell contains '1', it indicates the start of a new island
+                if grid[i][j]=="1": 
                     res+=1
-                    self.dfs(i,j,grid)
+                    self.dfs(i,j,grid) # Perform DFS to mark all connected land cells as visited
                     
         return res
     
     def dfs(self,r,c,grid):
+        # Check boundaries and if the cell is water or already visited
         if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]) or grid[r][c]!="1":
             return
         
-        grid[r][c]="0"   #mark them visited so that you dont get into infinited recursive call
+        grid[r][c]="0"   #  # Mark the cell as visited by setting it to '0' ..imp
+        
+        # Recursively visit all adjacent cells (up, down, left, right)
         self.dfs(r+1,c,grid)
         self.dfs(r-1,c,grid)
         self.dfs(r,c+1,grid)

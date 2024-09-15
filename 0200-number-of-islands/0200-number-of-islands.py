0d4a69d98e5f73fe4529c7bd12c49258b238b100
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-14 19:55:18 -0700

    Time: 233 ms (77.75%), Space: 18.9 MB (82.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,26 @@
-from typing import List
-
 class Solution:
-
-    def numIslands(self, grid: List[List[int]]) -> int:
-        # Check if the grid is empty
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or not grid[0]:
             return 0
-
-        row = len(grid)
-        col = len(grid[0])
-        res = 0
-
+        
+        row=len(grid)
+        col=len(grid[0])
+        res=0
+        
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == "1":
-                    res += 1
-                    self.dfs(i, j, grid)
-
+                if grid[i][j]=="1":
+                    res+=1
+                    self.dfs(i,j,grid)
+                    
         return res
-
-    def dfs(self, r: int, c: int, grid: List[List[int]]) -> None:
-        if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0":
+    
+    def dfs(self,r,c,grid):
+        if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]) or grid[r][c]!="1":
             return
-        grid[r][c] = "0"
-        self.dfs(r + 1, c, grid)
-        self.dfs(r - 1, c, grid)
-        self.dfs(r, c + 1, grid)
-        self.dfs(r, c - 1, grid)
+        
+        grid[r][c]="0"   #mark them visited so that you dont get into infinited recursive call
+        self.dfs(r+1,c,grid)
+        self.dfs(r-1,c,grid)
+        self.dfs(r,c+1,grid)
+        self.dfs(r,c-1,grid)

de4df152ea7bffca93cf816fd1e861f00c40700e
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-26 19:38:02 -0700

    Time: 43 ms (57.49%), Space: 17 MB (6.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,26 +1,30 @@
 class Solution:
+    """
+    Time Complexity: O(logN), N = size of the given array.
+    Reason: We are using binary search to search the target.
+    Space Complexity: O(1)
+    """
+
     def search(self, nums: List[int], target: int) -> int:
-        start=0
-        end=len(nums)-1
-        
-        while start<=end:
-            
-            mid=(start+end) // 2
-            
-            if nums[mid]==target:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            # Case 1: find target
+            if nums[mid] == target:
                 return mid
-            
-            if nums[start]<=nums[mid]:
-                if target>=nums[start] and target<=nums[mid]:
-                    end=mid-1
+
+            # Case 2: subarray on mid's left is sorted
+            if nums[start] <= nums[mid]:
+                if target >= nums[start] and target <= nums[mid]:
+                    end = mid - 1  # element exist
                 else:
-                    start=mid+1
-            
-            if nums[mid]<=nums[end]:
-                if target>=nums[mid] and target<=nums[end]:
-                    start=mid+1
+                    start = mid + 1  # element doesn't exist
+
+            # Case 3: subarray on mid's right is sorted.
+            if nums[mid] <= nums[end]:
+                if target >= nums[mid] and target <= nums[end]:
+                    start = mid + 1  # element exist
                 else:
-                    end=mid-1
-                    
+                    end = mid - 1  # element doesn't exist
         return -1
-                    

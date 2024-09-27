d16c394a0127afca43b7b086d972b22dcde363b8
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-26 18:02:27 -0700

    Time: 58 ms (17.20%), Space: 21.2 MB (26.45%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,38 +6,16 @@
 #         self.right = None
 
 class Solution:
-
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-
-        # Stack for tree traversal
-        stack = [root]
-
-        # Dictionary for parent pointers
-        parent = {root: None}
-
-        # Iterate until we find both the nodes p and q
-        while p not in parent or q not in parent:
-
-            node = stack.pop()
-
-            # While traversing the tree, keep saving the parent pointers.
-            if node.left:
-                parent[node.left] = node
-                stack.append(node.left)
-            if node.right:
-                parent[node.right] = node
-                stack.append(node.right)
-
-        # Ancestors set() for node p.
-        ancestors = set()
-
-        # Process all ancestors for node p using parent pointers.
-        while p:
-            ancestors.add(p)
-            p = parent[p]
-
-        # The first ancestor of q which appears in
-        # p's ancestor set() is their lowest common ancestor.
-        while q not in ancestors:
-            q = parent[q]
-        return q
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root==None or root==p or root==q:
+            return root
+        
+        left=self.lowestCommonAncestor(root.left,p,q)
+        right=self.lowestCommonAncestor(root.right,p,q)
+        
+        if left==None:
+            return right
+        elif right==None:
+            return left
+        else:
+            return root

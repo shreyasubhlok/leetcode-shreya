1138eabc93e6b1a26a3f5f75c187f50403721b18
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-26 18:03:14 -0700

    Time: 53 ms (39.66%), Space: 21.1 MB (26.45%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -5,17 +5,21 @@
 #         self.left = None
 #         self.right = None
 
+
+#https://www.youtube.com/watch?v=_-QHfMDde90 - reference (striver)
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root==None or root==p or root==q:
+    # Time complexity: O(N),N is no of nodes in tree. .
+    # Space Complexity: O(H),H is the height of the tree.
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if root == None or root == p or root == q:
             return root
-        
-        left=self.lowestCommonAncestor(root.left,p,q)
-        right=self.lowestCommonAncestor(root.right,p,q)
-        
-        if left==None:
+        # Recursive search for p and q in left and right subtrees
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        # If the left subtree returned None, it means p and q are not found in the left subtree.Therefore, the answer must be in the right subtree.
+        if left == None:
             return right
-        elif right==None:
+        elif right == None:  # if the right subtree returned None, the answer must be in the left subtree.
             return left
         else:
-            return root
+            return root  # If both left and right are not None, it means p and q were found in both subtrees.Hence, the current node is the LCA.

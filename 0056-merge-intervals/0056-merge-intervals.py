46061f68e253b4391bcbfb33945eb74ffd850f0b
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-26 19:01:11 -0700

    Time: 122 ms (64.86%), Space: 21.6 MB (9.07%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,19 +1,26 @@
 class Solution:
+    # time complexity: o(nlogn) due to sorting
+    # space complexity : o(n) for storing the merged intervals
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals=sorted(intervals, key=lambda interval:interval[0])
+        intervals = sorted(intervals, key=lambda interval: interval[0])
         print(intervals)
         
-        res=[]
-        start=intervals[0][0]
-        end=intervals[0][1]
+        res = []
+        start = intervals[0][0]
+        end = intervals[0][1]
         
-        for i in range(1,len(intervals)):
-            if intervals[i][0]<=end:
-                end=max(end,intervals[i][1])
+        # Iterate through the sorted intervals from index 1
+        for i in range(1, len(intervals)):
+            
+            # If overlaps. Update end to the maximum of the current end and the end of the overlapping interval
+            if intervals[i][0] <= end:
+                end = max(end, intervals[i][1])
             else:
-                res.append([start,end])
-                start=intervals[i][0]
-                end=intervals[i][1]
-        
-        res.append([start,end])
+                # No overlap, add the merged interval to the result. Update start and end to the current interval
+                res.append([start, end])
+                start = intervals[i][0]
+                end = intervals[i][1]
+
+        # Add the last merged interval
+        res.append([start, end])
         return res

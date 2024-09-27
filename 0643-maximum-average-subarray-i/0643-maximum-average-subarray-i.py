6f3ff75a6a8a9ba1916bc4deba15a4c3a2b3b95e
shreyasubhlok <shreyasubhlok@gmail.com>
2024-09-26 18:07:15 -0700

    Update 0643-maximum-average-subarray-i.py

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,23 +1,19 @@
 class Solution:
+    #Slidinf window fixed size problem
+    # Time : o(n)
+    # space:o(1)
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        maxAvg = -inf
-        i = 0
-        j = 0
-        w = 0
-        total = 0
+        if len(nums) < k:
+            return -1
+        windowSum = 0
+        # Calculate the sum of the first 'k' elements (initial window)
+        for i in range(k):
+            windowSum = windowSum + nums[i]
+        maxsum = windowSum  # Initialize maxSum with the sum of the first window
 
-        while j < len(nums):
-            total += nums[j]
-            w += 1
+        # Slide the window across the array and calculate new sums
+        for i in range(k, len(nums)):  # start from kth index i.e 4 and The loop iterates from k to len(nums) - 1
+            windowSum = windowSum + nums[i] - nums[i - k]
+            maxsum = max(windowSum, maxsum)
+        return maxsum / k  # Use normal division to return a float. '//' is integer division and truncates the decimal portion.
 
-            if w == k:
-                maxAvg = max(total, maxAvg)
-                total -= nums[i]
-                i += 1
-                w -= 1
-
-            j += 1
-
-
-        return maxAvg/k
-        

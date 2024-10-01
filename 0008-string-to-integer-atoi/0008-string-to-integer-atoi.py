710a357e2a13ae1ec8c31e8a893c3f6e9180e17b
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-01 14:40:28 -0700

    Time: 46 ms (9.72%), Space: 16.6 MB (65.16%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,35 +1,42 @@
 class Solution:
+    # time: o(n)
+    # space: o(1)
     def myAtoi(self, s: str) -> int:
-        # case whitespace handling
+        # Step 1: Handle leading whitespace by removing spaces at the beginning
         s = s.lstrip()
+
+        # If the string is empty after removing leading spaces, return 0
         if not s:
             return 0
 
-        # case sign handling
+        # Step 2: Handle the sign
         i = 0
-        sign = 1
-        if s[i] == '+':
+        sign = 1  # Default sign is positive
+        if s[i] == '+':  # If the first character is '+', move to the next character
             i = i + 1
-        elif s[i] == '-':
+        elif s[i] == '-':  # If the first character is '-', set sign to negative and move to the next character
             i = i + 1
             sign = -1
 
-        # convertion
-        parsed = 0
+        # Step 3: Convert the remaining part to an integer
+        parsed = 0  # Variable to hold the resulting integer value
         while i < len(s):
             curr = s[i]
-            if not curr.isdigit():
+            if not curr.isdigit():  # If the current character is not a digit, stop the conversion
                 break
             else:
                 parsed = parsed * 10 + int(curr)
             i = i + 1
 
+        # Multiply the parsed number by its sign
         parsed = parsed * sign
 
-        # rounding
+        # Step 4: Handle overflow and underflow cases
+        # If the parsed value exceeds the 32-bit signed integer range, return the appropriate boundary value
         if parsed > 2 ** 31 - 1:
             return 2 ** 31 - 1
-        elif parsed <= -2 ** 31:
+        elif parsed <= -2 ** 31 - 1:
             return -2 ** 31
         else:
             return parsed
+

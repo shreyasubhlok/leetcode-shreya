e8274526e15e8e86df34d9c7d46a6e59ea7a0241
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-02 01:45:17 -0700

    Time: 93 ms (33.77%), Space: 21.2 MB (69.62%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        # Step 1: Create a frequency counter map
+        counterMap = {}
+        for i in range(len(nums)):
+            if nums[i] in counterMap.keys():
+                counterMap[nums[i]] += 1
+            else:
+                counterMap[nums[i]] = 1
+        print(counterMap)
+
+        minHeap = []  # Initialize a min-heap to store the top k frequent elements
+        # Step 2: Use a min-heap to maintain the top k frequent elements
+        for num, count in counterMap.items():
+            heapq.heappush(minHeap, (count, num))  # Push the current (count, num) pair onto the min-heap
+            # If the heap size exceeds k, remove the least frequent element. Smallest count freq will be at root of heap
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+
+        # Step 3: Extract the numbers from the min-heap into the result list
+        while minHeap:
+            count, num = heapq.heappop(minHeap)  # Get the least frequent element
+            res.append(num)  # Add the number to the result list
+
+        return res

b5819866d94d30092c70769fc9ae8351d1f0b773
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-03 16:19:36 -0700

    Time: 92 ms (56.99%), Space: 16.5 MB (88.34%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        if board is None:
+            return False
+
+        squares = [set() for i in range(9)]
+
+        for r in range(9):
+            rows = set()
+            cols = set()
+            for c in range(9):
+
+                if board[r][c] in rows:
+                    return False
+                if board[r][c]!=".":
+                    rows.add(board[r][c])
+                
+                if board[c][r] in cols:
+                    return False
+                if board[c][r]!=".":
+                    cols.add(board[c][r])
+                
+                   
+                location = 3 * (r//3) + (c//3)
+                if board[r][c] in squares[location]:
+                    return False
+                if board[r][c]!=".":
+                    squares[location].add(board[r][c])
+                
+
+        return True

0c0dbec4320955dd67d216d235f91d681f37cb65
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-03 18:16:48 -0700

    Time: 538 ms (23.12%), Space: 29.4 MB (67.02%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,13 +1,19 @@
 class Solution:
+    '''
+    Time Complexity: O(n) because each element in the list is processed at most once.
+    Space Complexity: O(1) as no extra space is used beyond a few variables.
+    TWO POINTER APPROACH QUESTION
+    '''
     def maxArea(self, height: List[int]) -> int:
         start=0
         end=len(height)-1
         maximumArea=0
         while start<end:
             width=end-start
+            # Find the height by taking the minimum of the two heights at the pointers
             currentHeight=min(height[start],height[end])
-            area=width*currentHeight
-            maximumArea=max(area,maximumArea)
+            area=width*currentHeight  # area=width*height
+            maximumArea=max(area,maximumArea)  # Update the maximum area if the current area is larger
             if height[start]<height[end]:
                 start+=1
             else:

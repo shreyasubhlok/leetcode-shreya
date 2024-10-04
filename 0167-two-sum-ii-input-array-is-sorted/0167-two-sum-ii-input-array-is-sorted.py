fc7aea66b505a5b4e4f5d3974a9655e65e6f3823
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-03 17:59:15 -0700

    Time: 105 ms (56.01%), Space: 17.8 MB (49.11%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,8 +5,8 @@ class Solution:
         while low<=high:
             if numbers[low]+numbers[high]==target:
                 return [low+1,high+1]
-            if numbers[low]+numbers[high]>target:
-                high=high-1
-            else:
+            if numbers[low]+numbers[high]<target:
                 low=low+1
+            else:
+                high=high-1
         return[-1,-1]

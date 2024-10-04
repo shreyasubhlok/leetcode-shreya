e79cc1daed8a8a45bcdc0329bc8ccd51abeffddc
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-03 18:01:53 -0700

    Time: 113 ms (14.92%), Space: 17.8 MB (49.11%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,14 @@
 class Solution:
+    # two pointer technique
+    # Time: o(n) and space=O(1) . In two sum porblem space is o(n) coz of hashmap
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        low=0
-        high=len(numbers)-1
-        while low<=high:
-            if numbers[low]+numbers[high]==target:
-                return [low+1,high+1]
-            if numbers[low]+numbers[high]<target:
-                low=low+1
+        low = 0
+        high = len(numbers) - 1
+        while low <= high:
+            if numbers[low] + numbers[high] == target:
+                return [low + 1, high + 1]
+            if numbers[low] + numbers[high] < target:
+                low = low + 1
             else:
-                high=high-1
-        return[-1,-1]
+                high = high - 1
+        return [-1, -1]

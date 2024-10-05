65b43d29e856862d1c59cc2903698db18a76f744
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-04 21:31:49 -0700

    Time: 38 ms (41.80%), Space: 16.7 MB (6.93%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,17 +1,40 @@
 class Solution:
+    # Time: o(nlogn)-> heapq.heapify(stones) takes O(n) and heappop/heappush operations takes O(log n). o(n) +o(logn)= o(nlogn)
+    # space: o(n)
     def lastStoneWeight(self, stones: List[int]) -> int:
-        min_heap = []
-        k = 2
-        while len(stones)!=1:
-            for stone in stones:
-                heapq.heappush(min_heap, stone)
-                if len(min_heap) > k:
-                    heapq.heappop(min_heap)
+        # Convert all stones to negative to simulate a max-heap using Python's min-heap
+        # This way, we can use heapq to always extract the heaviest stone
+        for i in range(len(stones)):
+            stones[i] = -stones[i]
 
-            diff = min_heap[1] - min_heap[0]
-            stones.append(diff)
-            stones.remove(min_heap[1])
-            stones.remove(min_heap[0])
-            min_heap=[]
+        # Convert the list into a heap (min-heap, but we use negative values to simulate max-heap)
+        # heapify creates min-heap by default in Python, which allows efficient access to the smallest element).
+        heapq.heapify(stones)  
+        
+        while len(stones) > 1:
+            # Pop the two heaviest stones (most negative values)
+            stone1 = heapq.heappop(stones)
+            stone2 = heapq.heappop(stones)
 
-        return stones[0]
+            # If the stones are not equal, push the remaining weight back into the heap
+            if stone1 != stone2:
+                diff = stone1 - stone2
+                heapq.heappush(stones, diff)
+
+        # If there is one stone left, return its positive weight
+        if len(stones) != 0:
+            return -1 * stones[0]
+
+        # If no stones are left, return 0
+        return 0
+
+
+'''
+Time Complexity
+heapify:
+The time complexity of heapify is O(n), where n is the number of elements in the list. 
+This operation builds a heap in a bottom-up manner, ensuring each subtree is heapified starting from the leaves.
+
+sort:
+Sorting a list typically takes O(n log n).
+'''

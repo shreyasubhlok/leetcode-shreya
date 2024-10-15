f880de99f9385bcc6df3e1ae7eee5e4c220b29f3
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-14 22:30:56 -0700

    Time: 66 ms (25.56%), Space: 16.6 MB (49.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,20 +1,29 @@
 class Solution:
+    # Time is o(n) and space is o(n)
+    # sliding window technique- variable length
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s)==0:
+
+        if len(s) == 0:
             return 0
-        
-        i=0
-        j=0
-        myset=set()
-        maxlen=0
-        
-        while j<len(s):
+
+        i = 0  # start pointer of the sliding window
+        j = 0  # end pointer of the sliding window
+        myset = set()  # Set to store unique characters in the current window
+        maxlen = 0
+
+        # Traverse the string using the end pointer
+        while j < len(s):
+            # If the current character at index end is not in the set
             if s[j] not in myset:
-                myset.add(s[j])
-                currlen=len(myset)
-                maxlen=max(currlen,maxlen)
-                j=j+1
+                myset.add(s[j])  # Add the character to the set (expand the window)
+                currlen = len(myset)  # Calculate the current length of the substring
+                # Update maxlen if the current length is greater
+                maxlen = max(currlen, maxlen)
+                j = j + 1
             else:
+                # If the character is a duplicate (already in the set)
+                # Remove the character at index start (shrink the window)
                 myset.remove(s[i])
-                i=i+1
+                i = i + 1
+
         return maxlen

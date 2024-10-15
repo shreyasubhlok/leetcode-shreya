2b6e5f08405306aa3fb8a0a7698eb99917b97be3
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-14 22:31:28 -0700

    Time: 559 ms (60.26%), Space: 31.8 MB (95.46%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,27 +1,32 @@
 class Solution:
+    # sliding window fixed length
+    # Time is o(n) and space is o(n)
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        mySet=set()
-        i=0
-        j=0
-        maxsum=0
-        windowsum=0
-        
-        while j<len(nums):
+        mySet = set()  # Set to store unique elements within the sliding window
+        i = 0  # slinding window start pointer
+        j = 0  # slinding window end pointer
+        maxsum = 0
+        windowsum = 0
+
+        while j < len(nums):
+            # If the current element (nums[j]) is not in the set, add it to the window (expand)
             if nums[j] not in mySet:
-                mySet.add(nums[j])
-                windowsum=windowsum+nums[j]
+                mySet.add(nums[j])  # add to track unique elements
+                windowsum = windowsum + nums[j]
+
+                # Check if the current window has exactly k element, update maxsum
+                if len(mySet) == k:
+                    maxsum = max(maxsum, windowsum)
+                    mySet.remove(nums[i])  # Subtract nums[i] from the window sum
+                    windowsum = windowsum - nums[i]  # Remove the leftmost element to slide the window forward
+                    i = i + 1
+
+                j = j + 1  # Expand the window by moving the right pointer forward
                 
-                if len(mySet)==k:
-                    maxsum=max(maxsum,windowsum)
-                    mySet.remove(nums[i])
-                    windowsum=windowsum-nums[i]
-                    i=i+1
-                
-                j=j+1
             else:
-                mySet.remove(nums[i])
-                windowsum=windowsum-nums[i]
-                i=i+1
-                
-                
+                # If nums[j] is already in the set (duplicate element), shrink the window
+                mySet.remove(nums[i])  # Remove nums[i] from the set to remove the duplicate
+                windowsum = windowsum - nums[i]  # Subtract nums[i] from the window sum
+                i = i + 1  # Move the left pointer forward to shrink the window
+
         return maxsum

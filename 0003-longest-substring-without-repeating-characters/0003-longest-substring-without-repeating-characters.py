12d6f0bbc50858e7bf662527707e91065b3f1378
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-14 22:17:59 -0700

    Time: 53 ms (69.26%), Space: 16.6 MB (49.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,26 +1,20 @@
 class Solution:
-    # Time is o(n) and space is o(n)
-    # sliding window technique
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # Initialize two pointers, i and j, to represent the current window's start and end
-        i = 0  # Start of the sliding window
-        j = 0  # End of the sliding window
-        mySet = set()  # Set to store unique characters in the current window
-        maxlen = 0  
+        if len(s)==0:
+            return 0
         
-        # Traverse the string using the 'j' pointer
-        while j < len(s):
-            # If the current character at 'j' is not already in the set, it means no repetition
-            if s[j] not in mySet:
-                mySet.add(s[j])  # Add the character to the set (expanding the window)
-                # Update maxlen with the length of the current window (size of the set)
-                maxlen = max(maxlen, len(mySet))
-                j = j + 1  # Move the 'j' pointer to expand the window
+        i=0
+        j=0
+        myset=set()
+        maxlen=0
+        
+        while j<len(s):
+            if s[j] not in myset:
+                myset.add(s[j])
+                currlen=len(myset)
+                maxlen=max(currlen,maxlen)
+                j=j+1
             else:
-                # If the character at 'j' is already in the set (repetition found),
-                # Remove the character at 'i' (start of the window) to shrink the window
-                if s[i] != s[j] or s[i] == s[j]:
-                    mySet.remove(s[i])
-                i = i + 1  # Move the 'i' pointer to shrink the window
-
+                myset.remove(s[i])
+                i=i+1
         return maxlen

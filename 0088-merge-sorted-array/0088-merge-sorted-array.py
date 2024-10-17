7186a066f1172396a5bddf04abd1105a99c15699
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-16 21:16:02 -0700

    Time: 37 ms (74.02%), Space: 16.5 MB (62.24%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,23 +1,32 @@
+from typing import List
+
+
 class Solution:
+    # time is o(n+m)
+    # space is o(1)
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i=m-1
-        j=n-1
-        k=m+n-1
-        
-        while i>=0 and j>=0:
-            if nums1[i]<nums2[j]:
-                nums1[k]=nums2[j]
-                j=j-1
+        # Start merging from the back of both nums1 and nums2
+        i = m - 1 # Pointer for the last valid element in nums1
+        j = n - 1 # Pointer for the last element in nums2
+        k = m + n - 1  # Pointer for the last position in nums1 where elements should go
+
+        # Merge from back to front to avoid overwriting nums1's values
+        while i >= 0 and j >= 0:
+            if nums1[i] < nums2[j]:
+                nums1[k] = nums2[j]
+                j = j - 1
             else:
-                nums1[k]=nums1[i]
-                i=i-1
-            k=k-1
-            
-        while j>=0:
-            nums1[k]=nums2[j]
-            j=j-1
-            k=k-1
-            
+                nums1[k] = nums1[i]
+                i = i - 1
+            k = k - 1
+
+        # If there are remaining elements in nums2, copy them to nums1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j = j - 1
+            k = k - 1
+
+

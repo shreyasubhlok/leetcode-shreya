42a946fff05b1351b4a98de5f38c7997d237310c
shreyasubhlok <shreyasubhlok@gmail.com>
2024-10-28 01:28:19 -0700

    Time: 124 ms (57.49%), Space: 74.4 MB (39.03%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,38 +1,31 @@
 class TimeMap:
 
     def __init__(self):
-        # Initialize the keystore dictionary to hold lists of [value, timestamp] pairs for each key
-        self.keystore={}
-    
-    #Time is o(1) and space o(n)
+        self.keystore = {}
+
     def set(self, key: str, value: str, timestamp: int) -> None:
-        # If the key does not exist in the keystore, initialize it with an empty list
         if key not in self.keystore:
-            self.keystore[key]=[]
-        # Append the value-timestamp pair to the list associated with the key
-        self.keystore[key].append([value,timestamp])
-        
-    #Time o(logn) and space o(1)
+            self.keystore[key] = []
+        self.keystore[key].append([value, timestamp])
+
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.keystore:
             return ""
-        
-        res=""
-        values=self.keystore[key] # Retrieve the list of [value, timestamp] pairs for the given key
-        l=0
-        r=len(values)-1
-        while l<=r:
-            mid=(l+r)//2
-            if values[mid][1]<=timestamp:
-                # If mid timestamp is <= target, move right to find later timestamps
-                res=values[mid][0]
-                l=mid+1
+
+        res = ""
+        values = self.keystore[key]
+        left = 0
+        right = len(values) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                left = mid + 1
             else:
-                # If mid timestamp is greater than target, move left
-                r=mid-1
-        
+                right = mid - 1
+
         return res
-                
+
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()

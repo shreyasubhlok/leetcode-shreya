99855ea0ba5b75e6572bba1dc9d98c4d0b2379e8
shreyasubhlok <shreyasubhlok@gmail.com>
2024-11-07 22:09:47 -0800

    Time: 0 ms (100.00%), Space: 17.1 MB (11.70%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -5,6 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Time :O(n) and space:O(n)
     def zigzagLevelOrder(self, root: TreeNode) -> list[list[int]]:
         if root == None:
             return []
@@ -13,6 +14,7 @@ class Solution:
         queue.append(root)
         level = 0
         res = []
+        # The while loop and for loop combined only end up processing each node a single time, without repeated or redundant work. That's why o(n) and not o(n^2)
         while len(queue) != 0:
             currLevelNodes = []
             for i in range(len(queue)):
@@ -20,12 +22,14 @@ class Solution:
                 if level % 2 == 0:
                     currLevelNodes.append(curr.val)
                 else:
+                    #Consider a tree level with nodes [1, 2, 3] at an odd level:
+                    # If we use insert(0, curr.val) for each node, the final currLevelNodes will be [3, 2, 1], which is the reverse order.
                     currLevelNodes.insert(0, curr.val)
                 if curr.left != None:
                     queue.append(curr.left)
 
                 if curr.right != None:
                     queue.append(curr.right)
-            level+=1
+            level += 1
             res.append(currLevelNodes)
         return res

07323ace72fbc974543f0a8b6d206c46eb430103
shreyasubhlok <shreyasubhlok@gmail.com>
2024-11-07 21:34:46 -0800

    Time: 77 ms (75.44%), Space: 27.2 MB (39.14%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -4,7 +4,9 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
+    # time o(n) and space O(1) - Better approach. But give first o(n) apprach and later write optimzed solution
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         lenA = 1
         lenB = 1
@@ -22,15 +24,30 @@ class Solution:
         currA = headA
         currB = headB
         if lenA > lenB:
-             for i in range(lenA-lenB):
-                currA=currA.next
+            for i in range(lenA - lenB):
+                currA = currA.next
         else:
-            for i in range(lenB-lenA):
-                currB=currB.next
-    
-        
-        while currA!=currB:
-            currA=currA.next
-            currB=currB.next
-            
+            for i in range(lenB - lenA):
+                currB = currB.next
+
+        while currA != currB:
+            currA = currA.next
+            currB = currB.next
+
         return currA
+
+    # Time is o(n) and space O(n) coz of set - Give this as first apprach
+    def getIntersectionNodeUsingSet(self, headA: ListNode, headB: ListNode) -> ListNode:
+        myset = set()
+        curr = headA
+        while curr != None:
+            myset.add(curr)
+            curr = curr.next
+
+        newCurr = headB
+        while newCurr != None:
+            if newCurr in myset:
+                return newCurr
+            newCurr = newCurr.next
+
+        return None

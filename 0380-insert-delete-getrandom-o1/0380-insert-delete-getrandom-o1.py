3985d9b68fd65b1575e456865195785edf7282c4
shreyasubhlok <shreyasubhlok@gmail.com>
2024-11-09 19:58:17 -0800

    Time: 86 ms (92.01%), Space: 56.2 MB (78.22%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,39 +1,30 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.numMap = {}  # Map to store the index of each value in numList
-        self.numList = []  # List to store values for quick random access
+        self.numMap={}
+        self.numList=[]
 
     def insert(self, val: int) -> bool:
-        # If val already exists in the set, return False
         if val in self.numMap:
             return False
-        # Otherwise, add the value to numMap and numList
-        self.numMap[val] = len(self.numList)  # Store the index in numMap
-        self.numList.append(val)  # Add to the list
+        self.numMap[val]=len(self.numList)
+        self.numList.append(val)
         return True
 
     def remove(self, val: int) -> bool:
-        # If val does not exist in the set, return False
         if val not in self.numMap:
             return False
-        # Retrieve the index of the value to be removed
-        idx = self.numMap[val]
-        # Get the last element in the list
-        lastval = self.numList[-1]
-        # Move the last element to the position of the element to be removed
-        self.numList[idx] = lastval
-        # Update the map with the new index of lastval
-        self.numMap[lastval] = idx
-        # Remove the last element from the list
+        idx=self.numMap[val]
+        lastval=self.numList[-1]
+        self.numList[idx]=lastval
+        self.numMap[lastval]=idx
         self.numList.pop()
-        # Delete the value from the map
         del self.numMap[val]
         return True
 
     def getRandom(self) -> int:
-        # Return a random element from numList
         return random.choice(self.numList)
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:

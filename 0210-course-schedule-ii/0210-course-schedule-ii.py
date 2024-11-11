50df67949b1580786664538409a8a85175dc48cd
shreyasubhlok <shreyasubhlok@gmail.com>
2024-11-10 21:08:00 -0800

    Time: 5 ms (42.21%), Space: 17.7 MB (98.88%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adjList=[[] for i in range(numCourses)]
-        inDegrees=[0]*len(adjList)
-        topoOrder=[]
-        queue=deque()
-        
+        adjList = [[] for i in range(numCourses)]
+        inDegrees = [0] * len(adjList)
+        topoOrder = []
+        queue = deque()
+
         for pair in prerequisites:
-            course=pair[0]
-            prereq=pair[1]
+            course = pair[0]
+            prereq = pair[1]
             adjList[prereq].append(course)
-            inDegrees[course]+=1
-            
+            inDegrees[course] += 1
+
         for i in range(len(inDegrees)):
-            if inDegrees[i]==0:
+            if inDegrees[i] == 0:
                 queue.append(i)
-            
-            
+
         while queue:
-            currCourse=queue.popleft()
+            currCourse = queue.popleft()
             topoOrder.append(currCourse)
-            
             for prereq in adjList[currCourse]:
-                inDegrees[prereq]-=1
-                
-                if inDegrees[prereq]==0:
+                inDegrees[prereq] -= 1
+
+                if inDegrees[prereq] == 0:
                     queue.append(prereq)
-                    
-        if len(topoOrder)==numCourses:
+
+        if len(topoOrder) == numCourses:
             return topoOrder
         else:
             return []
-        

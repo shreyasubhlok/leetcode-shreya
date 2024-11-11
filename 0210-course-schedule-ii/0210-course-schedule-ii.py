1f33d1205108e25882d73cbe489a7b436e55eee5
shreyasubhlok <shreyasubhlok@gmail.com>
2024-11-10 20:45:54 -0800

    Time: 3 ms (78.41%), Space: 17.7 MB (99.83%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adjList=[[] for i in range(numCourses)]
+        inDegrees=[0]*len(adjList)
+        topoOrder=[]
+        queue=deque()
+        
+        for pair in prerequisites:
+            course=pair[0]
+            prereq=pair[1]
+            adjList[prereq].append(course)
+            inDegrees[course]+=1
+            
+        for i in range(len(inDegrees)):
+            if inDegrees[i]==0:
+                queue.append(i)
+            
+            
+        while queue:
+            currCourse=queue.popleft()
+            topoOrder.append(currCourse)
+            
+            for prereq in adjList[currCourse]:
+                inDegrees[prereq]-=1
+                
+                if inDegrees[prereq]==0:
+                    queue.append(prereq)
+                    
+        if len(topoOrder)==numCourses:
+            return topoOrder
+        else:
+            return []
+        

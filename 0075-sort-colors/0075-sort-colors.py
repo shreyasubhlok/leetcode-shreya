31e611ab341d3c3b0b1df74d949179ca60247e7a
shreyasubhlok <shreyasubhlok@gmail.com>
2025-01-20 21:09:32 -0800

    Time: 0 ms (100%), Space: 17.8 MB (33.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -9,12 +9,14 @@ class Solution:
 
         while mid <= high:
             if nums[mid] == 0:
+                # Swap the current element with the element at low
                 nums[mid], nums[low] = nums[low], nums[mid]
-                mid += 1
-                low += 1
+                low += 1  # Move low forward
+                mid += 1  # Move mid forward
             elif nums[mid] == 1:
-                mid += 1
+                mid += 1 # Just move mid forward
             else:
+                # Swap the current element with the element at high
                 nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
+                high -= 1  # Move high backward
         
